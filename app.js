36aee43ab044d1944183bd36b2ac4a0ed946595c
freejm290-stack <freejm290@gmail.com>
2026-06-12 17:58:45 +0300

    app.js

--- app.js
+++ app.js
@@ -1,55 +1,101 @@
 const products = [
 
 {
-name:"Диван прямой",
-category:"Диваны прямые",
-price:"24000",
-meters:"12",
-image:"https://picsum.photos/400/300?1"
+name: "Диван прямой",
+category: "Диваны прямые",
+price: "24000",
+meters: "12",
+image: "https://picsum.photos/400/300?1"
 },
 
 {
-name:"Кресло парикмахерское",
-category:"Кресла парикмахерские",
-price:"15000",
-meters:"5",
-image:"https://picsum.photos/400/300?2"
+name: "Кресло парикмахерское",
+category: "Кресла парикмахерские",
+price: "15000",
+meters: "5",
+image: "https://picsum.photos/400/300?2"
 }
 
 ];
 
-const container =
+const productsContainer =
 document.getElementById("products");
 
-products.forEach(product => {
+function renderProducts(items){
 
 ```
-container.innerHTML += `
-<div class="card">
+productsContainer.innerHTML = "";
 
-    <img src="${product.image}">
+items.forEach(product => {
 
-    <div class="card-body">
+    productsContainer.innerHTML += `
+        <div class="card" data-category="${product.category}">
 
-        <h3>${product.name}</h3>
+            <img src="${product.image}">
 
-        <div class="price">
-            ${product.price} ₽
+            <div class="card-body">
+
+                <h3>${product.name}</h3>
+
+                <div class="price">
+                    ${product.price} ₽
+                </div>
+
+                <div class="meters">
+                    Необходимый метраж:
+                    ${product.meters} м
+                </div>
+
+                <a href="#" class="crm-link">
+                    Открыть в CRM
+                </a>
+
+            </div>
+
         </div>
+    `;
 
-        <div class="meters">
-            Необходимый метраж:
-            ${product.meters} м
-        </div>
+});
+```
 
-        <a href="#" class="crm-link">
-            Открыть в CRM
-        </a>
+}
 
-    </div>
+function filterCategory(category){
 
-</div>
-`;
+```
+if(category === "all"){
+    renderProducts(products);
+    return;
+}
+
+const filtered =
+products.filter(item =>
+    item.category === category
+);
+
+renderProducts(filtered);
+```
+
+}
+
+document
+.getElementById("search")
+.addEventListener("input", function(){
+
+```
+const value =
+this.value.toLowerCase();
+
+const filtered =
+products.filter(item =>
+    item.name
+    .toLowerCase()
+    .includes(value)
+);
+
+renderProducts(filtered);
 ```
 
 });
+
+renderProducts(products);
